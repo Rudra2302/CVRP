1402b90ec5cb2bdb25fe6b36ebfb3fd14c667b36
Saptarshi <cs20b068@smail.iitm.ac.in>
2023-12-11 18:08:56 +0530

    made changes1

--- genInp.py
+++ genInp.py
@@ -1,17 +1,20 @@
 import random
+import sys
 # define a set
 s = set()
+n = int(sys.argv[1])
+N = str(n)
 
 print("NAME : inp1.vrp")
 print("COMMENT : input file for vrp")
 print("TYPE : CVRP")
-print("DIMENSION : 50000")
+print("DIMENSION : " + N)
 print("EDGE_WEIGHT_TYPE : EUC_2D")
 print("CAPACITY : 500000")
 print("NODE_COORD_SECTION")   
 
 
-for i in range(1,50001):
+for i in range(1,n+1):
     # generate a number uniformly in [1, 400]
     x = random.randint(-10000, 10000)
     y = random.randint(-10000, 10000)
@@ -30,7 +33,7 @@ for (x, y) in s:
     count += 1
 
 print("DEMAND_SECTION")
-for i in range(1, 50001):
+for i in range(1, n+1):
     print(str(i) + ' ' + str(random.randint(1, 500)))
 
 print("DEPOT_SECTION")
